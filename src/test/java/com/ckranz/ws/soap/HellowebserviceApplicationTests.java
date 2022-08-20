7dab690b6c6c6c287970b71818fc8beb3d2dacf6
Roman <romanfirs@list.ru>
2022-08-20 15:51:15 +0800

    first commit

---
+++ src/test/java/com/ckranz/ws/soap/HellowebserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ckranz.ws.soap;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HellowebserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
